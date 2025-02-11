f34359ed79e81282fbe69e63af4a18939e5a0787
Marcos D'França <marcosdfrancass@gmail.com>
2025-02-11 18:18:45 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(valorA, valorB) {
+    return valorA * valorB;
+}
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+console.log(multiplica(2, 2));
+console.log(saudacao('DFrança'));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplica(valorA: number, valorB: number): number {
+    return valorA * valorB;
+  }
+  
+  function saudacao(nome: string):string {
+    return 'Olá ' + nome;
+  }
+  
+  console.log(multiplica(2,2));
+  console.log(saudacao('DFrança'));
